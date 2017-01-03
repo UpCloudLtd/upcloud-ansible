12720cbd22a6f99316193e642c82dcd5d8885c7a
Ville Savolainen <ville.savolainen@dovecot.fi>
2017-01-03 17:56:07 +0200

    return also private ip

--- modules/upcloud.py
+++ modules/upcloud.py
@@ -243,7 +243,7 @@ def run(module, server_manager):
 
         server.ensure_started()
 
-        module.exit_json(changed=changed, server=server.to_dict(), public_ip=server.get_public_ip())
+        module.exit_json(changed=changed, server=server.to_dict(), public_ip=server.get_public_ip(), private_ip=server.get_private_ip())
 
     elif state == 'absent':
         server = server_manager.find_server(uuid, hostname)
