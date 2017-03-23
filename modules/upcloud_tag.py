7388001a1f86b8e9d83ad9689b9d3d43b9d70c0c
Onni Hakala <onni@keksi.io>
2017-03-23 09:00:40 +0200

    Add ability to change upcloud tags based on IP-address

--- modules/upcloud_tag.py
+++ modules/upcloud_tag.py
@@ -37,10 +37,13 @@ options:
         - UpCloud API password. Can be set as environment variable.
     hostname:
         description:
-        - Hostname of the target server to be (un)tagged. Hostname or uuid is needed.
+        - Hostname of the target server to be (un)tagged. Hostname, IP-address or uuid is needed.
+    ip_address:
+        description:
+        - IP address of the target server to be (un)tagged. Hostname, IP-address or uuid is needed.
     uuid:
         description:
-        - UUID of the target server to be (un)tagged. Hostname or uuid is needed.
+        - UUID of the target server to be (un)tagged. Hostname, IP-address or uuid is needed.
     tags:
         description:
         - List of tags (strings)
@@ -76,6 +79,8 @@ EXAMPLES = '''
 '''
 
 from distutils.version import LooseVersion
+from upcloud_api.errors import UpCloudClientError, UpCloudAPIError
+
 import os
 
 # make sure that upcloud-api is installed
@@ -111,7 +116,7 @@ class TagManager():
             if given_tag not in upcloud_tags:
                 self.manager.create_tag(given_tag)
 
-    def determine_server_uuid(self, hostname):
+    def determine_server_uuid_by_hostname(self, hostname):
         """
         Return uuid based on hostname.
         Fail if there are duplicates of the given hostname.
@@ -131,6 +136,20 @@ class TagManager():
         else:
             self.module.fail_json(msg='No server was found with hostname: ' + hostname)
 
+    def determine_server_uuid_by_ip(self, ip_address):
+        """
+        Return uuid based on IP-address.
+        Fail if Upcloud doesn't know the IP-address
+        """
+        try:
+            machine = self.manager.get_server_by_ip(ip_address)
+            return machine.uuid
+        except UpCloudAPIError as e:
+            if e.error_code == 'IP_ADDRESS_NOT_FOUND':
+                self.module.fail_json(msg='No server was found with IP-address: ' + ip_address)
+            else:
+                raise
+
     def get_host_tags(self, uuid):
         host_tags = self.manager.get_server(uuid).tags
         return [ str(host_tag) for host_tag in host_tags ]
@@ -147,15 +166,19 @@ def run(module, tag_manager):
         (don't remove them from upcloud's available tags, might be present in other servers)
     """
 
-    state =     module.params['state']
-    tags =      module.params['tags']
-    uuid =      module.params.get('uuid')
-    hostname =  module.params.get('hostname')
+    state =         module.params['state']
+    tags =          module.params['tags']
+    uuid =          module.params.get('uuid')
+    hostname =      module.params.get('hostname')
+    ip_address =    module.params.get('ip_address')
 
     changed = False
 
     if not uuid:
-        uuid = tag_manager.determine_server_uuid(hostname)
+        if hostname:
+            uuid = firewall_manager.determine_server_uuid_by_hostname(hostname)
+        elif ip_address:
+            uuid = firewall_manager.determine_server_uuid_by_ip(ip_address)
 
     # make sure the host has all tags
     if state == 'present':
@@ -199,11 +222,12 @@ def main():
             api_passwd = dict(aliases=['UPCLOUD_API_PASSWD'], no_log=True),
 
             hostname = dict(type='str'),
+            ip_address = dict(type='str'),
             uuid = dict(aliases=['id'], type='str'),
             tags = dict(type='list', required=True)
         ),
         required_one_of = (
-            ['uuid', 'hostname'],
+            ['uuid', 'hostname', 'ip_address'],
         )
     )
 
